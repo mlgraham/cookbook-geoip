4041ce3a65f2c448daeba4fbaeac72c098c5282b
Michael Graham <michaelgraham@Michaels-MacBook-Pro.local>
2016-12-13 16:33:24 -0500

    Updated download URLs

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-default['geoip']['country']['url'] = 'https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz';
+default['geoip']['country']['url'] = 'https://github.com/mlgraham/cookbook-geoip/raw/2d2f09d456fd58071c13ea7e3504d2449915cb6a/downloads/GeoLite2-Country.mmdb.gz';
 default['geoip']['country']['file'] = '/usr/local/share/GeoIP/GeoLite2-Country.mmdb';
 
-default['geoip']['city']['url'] = 'https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz';
+default['geoip']['city']['url'] = 'https://github.com/mlgraham/cookbook-geoip/raw/2d2f09d456fd58071c13ea7e3504d2449915cb6a/downloads/GeoLite2-City.mmdb.gz';
 default['geoip']['city']['file'] = '/usr/local/share/GeoIP/GeoLite2-City.mmdb';
